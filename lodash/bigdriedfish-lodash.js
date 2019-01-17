42338deb2185dafed5e22cff573ccb58dad9b7b5
xiaoyugan <1690439769@qq.com>
2019-01-17 23:00:41 +0800

    lodashfirstcommit

---
+++ lodash/bigdriedfish-lodash.js
@@ -0,0 +1,18 @@
+var bigdriedfish = {
+  chunk: function chunk(ary, size) {
+    let res = []
+    for (let i = 0; i < arr.length; i += size) {
+        res.push(arr.slice(i, i + size))
+    }
+    return res
+  },
+  compat: function compact(array) {
+    let surplus = []
+    for (let i = 0; i < array.length; i++) {
+      if (array[i]) {
+        surplus.push(array[i])
+      }
+    }
+    return surplus
+  }
+}
