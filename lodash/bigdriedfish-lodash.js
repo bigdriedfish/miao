73642ba959b1c731ec21c9beb6558c89e9c48182
xiaoyugan <1690439769@qq.com>
2019-01-18 16:16:58 +0800

    begin lodash

--- lodash/bigdriedfish-lodash.js
+++ lodash/bigdriedfish-lodash.js
@@ -1,7 +1,7 @@
 var bigdriedfish = (function (){
   function chunk(ary, size) {
     let res = []
-    for (let i = 0; i < arr.length; i += size) {
+    for (let i = 0; i < ary.length; i += size) {
         res.push(arr.slice(i, i + size))
     }
     return res
