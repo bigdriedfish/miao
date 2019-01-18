d44880797f7fbd173a08ff1b1a60c66ebe0f8d93
xiaoyugan <1690439769@qq.com>
2019-01-18 16:07:22 +0800

    failed again

--- lodash/bigdriedfish-lodash.js
+++ lodash/bigdriedfish-lodash.js
@@ -1,12 +1,12 @@
-var bigdriedfish = function() {
-  function chunk(ary, size) {
+var bigdriedfish = {
+  chunk: function chunk(ary, size) {
     let res = []
     for (let i = 0; i < arr.length; i += size) {
         res.push(arr.slice(i, i + size))
     }
     return res
-  }
-  function compact(array) {
+  },
+  compat: function compact(array) {
     let surplus = []
     for (let i = 0; i < array.length; i++) {
       if (array[i]) {
@@ -14,18 +14,11 @@ var bigdriedfish = function() {
       }
     }
     return surplus
-  }
+  },
   
-
-
-
-
-  reuturn {
-    chunk: chunk,
-    compact: compact,
+  return {
+    chunk,
+    compact
   }
-
-
-
-}()
+}
 
